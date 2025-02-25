364113295d97822f9efa53538321f9bf3c88476c
Nitesh Balusu <niteshbalusu@icloud.com>
2025-02-25 17:40:28 -0500

    remove unused code

--- cpp/HybridMath.cpp
+++
@@ -1,5 +0,0 @@
-#include "HybridMath.hpp"
-
-namespace margelo::nitro::nitrotor {
-  double HybridMath::add(double a, double b) { return a + b + 100; }
-} // namespace margelo::nitro::nitrotor
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
 import { NitroModules } from 'react-native-nitro-modules';
-import type { Math as MathSpec } from './Math.nitro';
 import type { Tor as TorSpec } from './Tor.nitro';
 
-export const RnMath = NitroModules.createHybridObject<MathSpec>('Math');
 export const RnTor = NitroModules.createHybridObject<TorSpec>('Tor');
